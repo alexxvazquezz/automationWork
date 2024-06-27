01d9ac849e03b3ef991472e2cafba244fc5138e9
Alejandro Vazquez <alexx.vazquezz@gmail.com>
2024-06-27 17:08:40 -0300

    added new tests and methods

--- HRPage/pages/base_page.py
+++ HRPage/pages/base_page.py
@@ -1,12 +1,20 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
 
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
     
-    def find_element(self, locator):
-        return WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(locator))
+    def find_element(self, locator, timeout=10):
+        try:
+            element = WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_element_located(locator)
+            )
+            return element
+        except TimeoutException:
+            raise NoSuchElementException(f"Element not found: {locator}") 
     
     def find_elements(self, locator):
         return WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located(locator))
--- HRPage/pages/user_management_page.py
+++ HRPage/pages/user_management_page.py
@@ -8,8 +8,8 @@ class UserManagement(BasePage):
     ADD_USER_BUTTON = (By.CSS_SELECTOR, ".oxd-button-icon")
     ADD_USER_USER_ROLE_DROPDOWN = (By.CSS_SELECTOR, 'div:nth-child(1) > div > div:nth-child(1) > div > div:nth-child(2) > div > div')
     USER_ROLE_ADMIN_INPUT_VALUE = (By.CSS_SELECTOR, "div.oxd-select-dropdown.--positon-bottom > div:nth-child(2)")
-    EMPLOYEE_INPUT_NAME = (By.CSS_SELECTOR, 'input[placeholder="Type for hints..."]')
-    EMPLOYEE_NAME_FIRST_ITEM = (By.CSS_SELECTOR, 'div.oxd-autocomplete-dropdown.--positon-bottom > div:nth-child(1)')
+    EMPLOYEE_INPUT_NAME = (By.CSS_SELECTOR, 'div:nth-child(1) > div > div:nth-child(2) > div > div:nth-child(2) > div > div > input')
+    EMPLOYEE_NAME_FIRST_ITEM_ADD = (By.CSS_SELECTOR, 'div.oxd-autocomplete-dropdown.--positon-bottom > div.oxd-autocomplete-option.--selected')
     STATUS_DROPDOWN = (By.CSS_SELECTOR, 'div:nth-child(3) > div > div:nth-child(2) > div > div > div.oxd-select-text-input')
     STATUS_ENABLED = (By.CSS_SELECTOR, 'div:nth-child(2) > div > div.oxd-select-dropdown.--positon-bottom > div:nth-child(2)')
     USERNAME_INPUT = (By.CSS_SELECTOR, 'div:nth-child(4) > div > div:nth-child(2) > input')
@@ -21,7 +21,15 @@ class UserManagement(BasePage):
     SHOULD_BE_5_CHARACTERS = (By.CSS_SELECTOR,'form > div:nth-child(1) > div > div:nth-child(4) > div > span')
     PASSWORDS_DO_NOT_MATCH = (By.CSS_SELECTOR, 'form > div.oxd-form-row.user-password-row > div > div:nth-child(2) > div > span')
     PASSWORD_STRENGTH_LABEL = (By.CSS_SELECTOR, 'div.oxd-grid-item.oxd-grid-item--gutters.user-password-cell > span')
-    USER_ALREADY_EXISTS_ERROR = (By.CSS_SELECTOR, 'oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message')
+    CANCEL_BUTTON = (By.CSS_SELECTOR, 'button.oxd-button.oxd-button--medium.oxd-button--ghost')
+    ALREADY_EXISTS = (By.CSS_SELECTOR, 'div:nth-child(1) > div > div:nth-child(4) > div > span')
+    SEARCH_INPUT_USERNAME = (By.CSS_SELECTOR, 'div:nth-child(2) > input')
+    USER_IN_TABLE = (By.CSS_SELECTOR, 'div.oxd-table-body > div.oxd-table-card')
+    SEARCH_BUTTON = (By.CSS_SELECTOR, 'button.oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-left-space')
+    USER_ROLE_DROPDOWN = (By.CSS_SELECTOR, 'div:nth-child(2) > div > div:nth-child(2) > div > div > div.oxd-select-text-input')
+    FIRST_ELEMENT_USER_TABLE = (By.CSS_SELECTOR, 'div.oxd-table-body > div:nth-child(2) > div > div:nth-child(4) > div')
+    SEARCH_INPUT_EMPLOYEE_NAME = (By.CSS_SELECTOR, 'div > div:nth-child(2) > div > div > input')
+    EMPLOYEE_NAME_DROPDOWN = (By.CSS_SELECTOR, 'div.oxd-autocomplete-dropdown.--positon-bottom > div > span')
 
 
     def __init__(self, driver):
@@ -43,7 +51,8 @@ class UserManagement(BasePage):
         self.find_element(self.EMPLOYEE_INPUT_NAME).send_keys(str)
 
     def click_value_employee_name(self):
-        self.find_element(self.EMPLOYEE_NAME_FIRST_ITEM).click()
+       element = self.find_element(self.EMPLOYEE_NAME_FIRST_ITEM_ADD)
+       element.click
 
     def click_status_dropdown(self):
         self.find_element(self.STATUS_DROPDOWN).click()
@@ -69,7 +78,6 @@ class UserManagement(BasePage):
         self.add_admin_input_value()
         self.enter_employee_name(employeeName)
         time.sleep(2)
-        self.click_value_employee_name()
         self.click_status_dropdown()
         self.click_status_enabled()
         self.add_username(userName)
@@ -105,6 +113,9 @@ class UserManagement(BasePage):
                 break
             i += 1
         return None
+    
+    def click_cancel(self):
+        self.find_element(self.CANCEL_BUTTON).click()
 
     def validate_required_all_fields(self):
         for i in range(1, 5):
@@ -116,6 +127,34 @@ class UserManagement(BasePage):
             except:
                 return False
         return True
+
+    def validate_user_role(self, user_role):
+        i = 1
+        while True:
+            try:
+                element = self.find_element((By.CSS_SELECTOR, f'div.oxd-table-body > div:nth-child({i}) > div > div:nth-child(3) > div'))
+                text = element.text.strip()
+                if text != user_role:
+                    return False
+            except NoSuchElementException:
+                break
+            i += 1
+        return True
+    
+
+    def validate_employee_name(self, employee_name):
+        i = 1
+        while True:
+            try:
+                element = self.find_element((By.CSS_SELECTOR, f'div.oxd-table-body > div:nth-child({i}) > div > div:nth-child(4) > div'))
+                text = element.text.strip()
+                if text != employee_name:
+                    return False
+            except NoSuchElementException:
+                break
+            i += 1
+        return True
+
 
     def validate_password_error(self):
         return self.find_element(self.PASSWORD_ERROR).text
@@ -134,10 +173,39 @@ class UserManagement(BasePage):
         password_input = self.find_element(self.PASSWORD_INPUT)
         password_input.clear()
 
-    def validate_already_exists_error(self):
-        return self.find_element(self.USER_ALREADY_EXISTS_ERROR)
+    def validate_already_exists_error(self, text):
+        return self.wait_for_text_change(self.ALREADY_EXISTS, text)
+    
+    def enter_username_search(self, str):
+        self.find_element(self.SEARCH_INPUT_USERNAME).send_keys(str)
+
+    def count_users_in_table(self):
+        rows = self.find_elements(self.USER_IN_TABLE)
+        return len(rows)
+    
+    def click_search(self):
+        self.find_element(self.SEARCH_BUTTON).click()
+
+    def select_search_user_role(self, role):
+        nth_child = 0
+        self.find_element(self.USER_ROLE_DROPDOWN).click()
+
+        if role == 'Admin':
+            nth_child = 2
+        if role == 'ESS':
+            nth_child = 3
+            
+        dropdown = self.find_element((By.CSS_SELECTOR, f'div.oxd-select-dropdown.--positon-bottom > div:nth-child({nth_child}) > span'))
+        dropdown.click()
+     
+    def get_first_employee_name(self):
+        employee_name = self.find_element(self.FIRST_ELEMENT_USER_TABLE).text
+        return employee_name
+    
+    def enter_employee_name(self, employee_name):
+        input = self.find_element(self.SEARCH_INPUT_EMPLOYEE_NAME)
+        input.send_keys(employee_name)
+        self.find_element(self.EMPLOYEE_NAME_DROPDOWN).click()
 
     
 
-
-    
